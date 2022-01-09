b8f17565e125fa05926b697416e7cb5dc48a0490
Stefan Machmeier <stefan-machmeier@outlook.com>
2022-01-09 20:42:53 +0100

    Fix typos

--- listings/auth-passwd.c
+++ listings/auth-passwd.c
@@ -2,12 +2,10 @@ int
 auth_password(struct ssh *ssh, const char *password)
 {
     Authctxt *authctxt = ssh->authctxt;
-
     /* Send the request to Cowrie */
     int rc;
     rc = authenticate_password(authctxt->user, password);
     authctxt->valid = 1;
-
     /* libssh returns different values compared to OpenSSH, for SSH_AUTH_SUCCESS=0 returns 1 */
     if (rc == 0)
     {
@@ -20,4 +18,19 @@ auth_password(struct ssh *ssh, const char *password)
     }
     /* end */
     ...
+}
+int authenticate_password(const char *username, const char *password)
+{
+    int rc = -1;
+    /* No logins if we could not connect to Cowrie */
+    if (ssh_client_conns1[0].error != 1)
+    {
+        rc = ssh_userauth_password(ssh_client_conns1[0].initial_session, username, password);
+    }
+    return rc;
+}
+int
+allowed_user(struct ssh *ssh, struct passwd * pw)
+{
+    return 1;
 }
